674ede3ce2d0e3e5757bbc184c5b49270cbf3bd5
fady gamil <fadygamilha@gmail.com>
2023-07-28 05:51:16 +0300

    import path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose  = require('mongoose');
+const path = require('path');
 
 const app = express();
 app.use(express.urlencoded({extended: 'false'}))
